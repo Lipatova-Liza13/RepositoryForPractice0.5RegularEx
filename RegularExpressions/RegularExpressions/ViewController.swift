8f0fbebf5704c5afd4501af5070f66856a31c22a
Lipatova-Liza13 <lipatovaliza13@gmail.com>
2020-08-12 13:56:54 +0300

    create project and add .gitignore

---
+++ RegularExpressions/RegularExpressions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegularExpressions
+//
+//  Created by Teslenko Anastasiya on 12.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RegularExpressions/RegularExpressionsTests/RegularExpressionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  RegularExpressionsTests.swift
+//  RegularExpressionsTests
+//
+//  Created by Teslenko Anastasiya on 12.08.2020.
+//  Copyright © 2020 Liza Lipatova. All rights reserved.
+//
+
+import XCTest
+@testable import RegularExpressions
+
+class RegularExpressionsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
